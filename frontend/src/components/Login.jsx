5b57ef585ad70467cd9816b90b0fae7e23ed8f7e
wannaflyhigh <joeydai901108@gmail.com>
2024-02-23 16:46:16 +0800

    Get access_token in Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,24 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 export default function Login() {
-	const { VITE_CLIENT_ID } = import.meta.env
+	const { VITE_CLIENT_ID, VITE_BACKEND_URL } = import.meta.env
+
+	useEffect(() => {
+		const code = new URL(location.href).searchParams.get("code")
+		if (code) {
+			fetch(VITE_BACKEND_URL + `/codeExchageAuthToken?code=${code}`)
+				.then((res) => res.json())
+				.then((data) => {
+					console.log(data)
+					localStorage.setItem("access_token", data.access_token)
+					location.search = ""
+				})
+		}
+	}, [])
 
 	return (
 		<a
-			href={`https://github.com/login/oauth/authorize?client_id=${VITE_CLIENT_ID}`}
+			href={`https://github.com/login/oauth/authorize?client_id=${VITE_CLIENT_ID}&redirect_uri=${location.href}`}
 		>
 			login to Github
 		</a>
